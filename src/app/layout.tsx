d132d312840e59882606c6500572c164fda8f87f
Ben Mallett <ben@Bens-MacBook-Air-2.local>
2024-07-03 01:57:14 -0400

    updating to force prettier change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,34 @@
-import type { Metadata } from "next";
-import Head from "next/head";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import Head from 'next/head';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Pontus",
-  description: "Control application for embedded aquarium sensors.",
+    title: 'Pontus',
+    description: 'Control application for embedded aquarium sensors.',
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <Head>
-        <title>Pontus</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <body
-        className={`bg-gradient-to-b from-blue-200 to-cyan-200 ${inter.className}`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <Head>
+                <title>Pontus</title>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+            </Head>
+            <body
+                className={`bg-gradient-to-b from-blue-200 to-cyan-200 ${inter.className}`}
+            >
+                {children}
+            </body>
+        </html>
+    );
 }
