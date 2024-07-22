8538016bf01d0a71299427cb530bc4d1ed6186e0
Ben Mallett <bencmallett@outlook.com>
2024-07-22 17:35:26 -0400

    changing seeding

--- src/lib/prisma/seed.ts
+++ src/lib/prisma/seed.ts
@@ -6,73 +6,48 @@ async function main() {
     await prisma.user.deleteMany();
     await prisma.device.deleteMany();
     await prisma.sensorReading.deleteMany();
-    const existingAdmin = await prisma.user.findUnique({
-        where: {
+
+    const adminUser = await prisma.user.create({
+        data: {
             email: 'admin@admin.io',
+            password:
+                '$2a$10$a3hqUKvcar52yVRRAehVReArIO06rUg48836hEAWwu1z5A6PFD532',
+            name: 'Ben',
+            role: Role.ADMIN,
+            createdAt: new Date(),
+            devices: {
+                create: generateDevices(),
+            },
         },
     });
-    const existingUser1 = await prisma.user.findUnique({
-        where: {
+
+    const user1 = await prisma.user.create({
+        data: {
             email: 'bob@pontus.io',
+            password:
+                '$2a$10$a3hqUKvcar52yVRRAehVReArIO06rUg48836hEAWwu1z5A6PFD532',
+            name: 'Bob',
+            role: Role.USER,
+            createdAt: new Date(new Date().setDate(new Date().getDate() + 1)),
+            devices: {
+                create: generateDevices(),
+            },
         },
     });
-    const existingUser2 = await prisma.user.findUnique({
-        where: {
+
+    const user2 = await prisma.user.create({
+        data: {
             email: 'alice@pontus.io',
+            password:
+                '$2a$10$a3hqUKvcar52yVRRAehVReArIO06rUg48836hEAWwu1z5A6PFD532',
+            name: 'Alice',
+            role: Role.USER,
+            createdAt: new Date(new Date().setDate(new Date().getDate() + 2)),
+            devices: {
+                create: generateDevices(),
+            },
         },
     });
-
-    if (existingAdmin === undefined) {
-        const adminUser = await prisma.user.create({
-            data: {
-                email: 'admin@admin.io',
-                password:
-                    '$2a$10$a3hqUKvcar52yVRRAehVReArIO06rUg48836hEAWwu1z5A6PFD532',
-                name: 'Ben',
-                role: Role.ADMIN,
-                createdAt: new Date(),
-                devices: {
-                    create: generateDevices(),
-                },
-            },
-        });
-    }
-
-    if (existingUser1 === undefined) {
-        const user1 = await prisma.user.create({
-            data: {
-                email: 'bob@pontus.io',
-                password:
-                    '$2a$10$a3hqUKvcar52yVRRAehVReArIO06rUg48836hEAWwu1z5A6PFD532',
-                name: 'Bob',
-                role: Role.USER,
-                createdAt: new Date(
-                    new Date().setDate(new Date().getDate() + 1)
-                ),
-                devices: {
-                    create: generateDevices(),
-                },
-            },
-        });
-    }
-
-    if (existingUser2 === undefined) {
-        const user2 = await prisma.user.create({
-            data: {
-                email: 'alice@pontus.io',
-                password:
-                    '$2a$10$a3hqUKvcar52yVRRAehVReArIO06rUg48836hEAWwu1z5A6PFD532',
-                name: 'Alice',
-                role: Role.USER,
-                createdAt: new Date(
-                    new Date().setDate(new Date().getDate() + 2)
-                ),
-                devices: {
-                    create: generateDevices(),
-                },
-            },
-        });
-    }
 }
 
 function generateDevices() {
